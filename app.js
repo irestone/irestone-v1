4b97f34e223997104f08a01f11fea1a54b0aef09
irestone <irestone.work@gmail.com>
2019-08-06 13:58:50 +0300

    Refactor: use esm instead of cjs

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
-var stylus = require('stylus')
+import createHTTPError from 'http-errors'
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import stylus from 'stylus'
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
+// routes
+import { indexRouter } from './routes/index'
+import { aboutRouter } from './routes/about'
 
-var app = express()
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+// setting up
+export const app = express()
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+// view engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+// static files
 app.use(stylus.middleware(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'public')))
 
+// routing
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/about', aboutRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404))
-})
+app.use((_, __, next) => next(createHTTPError(404)))
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use((err, req, res, next) => {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
   res.status(err.status || 500)
   res.render('error')
 })
-
-module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from 'express'
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+export const indexRouter = new Router()
 
-module.exports = router;
+indexRouter.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' })
+})
