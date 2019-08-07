8b88e1fe65bee3b649b1c42b789d30b3914c0a62
irestone <irestone.work@gmail.com>
2019-08-07 17:49:34 +0300

    Add bundling w/ Rollup

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import resolve from 'rollup-plugin-node-resolve'
+import cjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+const scriptsDir = 'public/scripts'
+console.log('rollup config')
+
+export default {
+  input: `${scriptsDir}/main.js`,
+  output: {
+    file: `${scriptsDir}/bundle.min.js`,
+    format: 'esm',
+  },
+  plugins: [resolve(), cjs(), terser()],
+  watch: { clearScreen: false },
+}
