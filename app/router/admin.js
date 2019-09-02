338d5406101724268441bdadca9f4a42edb6a9e3
irestone <irestone.work@gmail.com>
2019-09-02 15:22:31 +0300

    Refactor naming

--- app/router/admin.js
+++ app/router/admin.js
@@ -1,33 +1,33 @@
-import path from 'path'
-import createHTTPError from 'http-errors'
 import { Router } from 'express'
+import createHTTPError from 'http-errors'
+import path from 'path'
 
 import { Page } from './models/Page'
 import { Category } from './models/Category'
 
-const adminViews = (fileName) => path.join('admin', fileName)
+const view = (name) => path.join('admin', name)
 
 export const adminRouter = new Router()
 
 adminRouter.get('/', (req, res) => {
-  res.render(adminViews('index'), {
+  res.render(view('index'), {
     title: 'Settings',
     slug: 'settings',
   })
 })
 
 adminRouter.get('/pages/new', (req, res) => {
-  res.render(adminViews('new_page'))
+  res.render(view('new_page'))
 })
 
 adminRouter.get('/pages/:slug', async (req, res, next) => {
   const page = await Page.findOne({ slug: req.params.slug })
   if (!page) return next(createHTTPError(404))
-  res.render(adminViews('page'), page)
+  res.render(view('page'), page)
 })
 
 adminRouter.get('/categories/:slug', async (req, res, next) => {
   const category = await Category.findOne({ slug: req.params.slug })
   if (!category) return next(createHTTPError(404))
-  res.render(adminViews('category'), category)
+  res.render(view('category'), category)
 })
